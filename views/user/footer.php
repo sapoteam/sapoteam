583c73cd16ad2ebd832e5a29b651e097c79496cc
Aotow6 <satria.rajawali3@gmail.com>
2026-04-29 19:52:58 +0800

    fix no wa on footer

--- views/user/footer.php
+++ views/user/footer.php
@@ -32,7 +32,7 @@
       <div class="col-md-4 col-lg-2">
         <h6 class="footer-title">Sosial Media</h6>
         <div class="social-wrapper">
-          <a href="https://wa.me/628115522124" class="social-icon" target="_blank"><i class="bi bi-whatsapp"></i></a>
+          <a href="https://wa.me/6282252962600" class="social-icon" target="_blank"><i class="bi bi-whatsapp"></i></a>
           <a href="https://instagram.com/oemahkeboen.smd" class="social-icon" target="_blank"><i class="bi bi-instagram"></i></a>
           <a href="https://www.facebook.com/oemahkeboen.smd" class="social-icon" target="_blank"><i class="bi bi-facebook"></i></a>
           <a href="https://www.tiktok.com/@oemahkeboensmd" class="social-icon" target="_blank"><i class="bi bi-tiktok"></i></a>
